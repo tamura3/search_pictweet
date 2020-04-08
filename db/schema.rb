e9a7d71439812b1840640c51bba1cf4b20218968
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-04-08 22:45:17 +0900

    textカラムに対するインデックスを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_014147) do
+ActiveRecord::Schema.define(version: 2020_04_08_133547) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_014147) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+    t.index ["text"], name: "index_tweets_on_text", length: 32
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
